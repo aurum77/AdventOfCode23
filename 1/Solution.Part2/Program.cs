a5665ced85cf5d3426ec8a532d65020931949564
aurum77 <58880138+aurum77@users.noreply.github.com>
2023-12-02 19:24:27 +0300

    add day 1 part 1

---
+++ 1/Solution.Part2/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string input = Directory.GetCurrentDirectory().ToString() + "/input.txt";
+Console.Write(input);
+
+var lines = File.ReadLines(input);
+int sum = 0;
+char first;
+char last;
+
+string strnum;
+
+foreach (var line in lines)
+{
+    if (line.Where(Char.IsDigit).ToList().Count != 0)
+    {
+        first = line.Where(Char.IsDigit).ToList().First();
+        last = line.Where(Char.IsDigit).ToList().Last();
+        strnum = $"{first}{last}";
+
+        sum += int.Parse(strnum);
+    }
+}
+
+Console.WriteLine(sum);
